3780e519971be930b7d3b1a990e87a58472c19d0
jey <jey@jeys-MacBook-Pro.local>
2020-01-08 01:33:21 +0300

    Add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home()
-    ));
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 30,
-          title: Text('Title'),
-          centerTitle: true,
-        ),
-        body: Center(child: Image.asset('assets/a.png'),),
-        backgroundColor: Colors.blue[50],
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.red[200],
-          elevation: 20,
-          child: Text('Click'),
-        ),
-      );
+      appBar: AppBar(
+        elevation: 30,
+        title: Text('Title'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.asset('assets/a.png'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 10,
+        child: RaisedButton.icon(onPressed: () {}, label: Text('Click'), icon: Icon(Icons.access_alarms,)),
+      ),
+      backgroundColor: Colors.blue[50],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red[200],
+        elevation: 20,
+        child: Text('Click'),
+      ),
+    );
   }
 }
